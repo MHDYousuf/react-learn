65334a5b6a179d497873646fc571331321ec92fa
Mohammed Yousuf <36731361+myousuf07@users.noreply.github.com>
2019-06-02 19:52:18 +0530

    added renderDish method

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -4,13 +4,37 @@ import { Card, CardImg, CardImgOverlay, CardBody, CardText, CardTitle } from 're
 class MenuComponent extends Component{
     constructor(props){
         super(props);
-        this.state = {}
+        this.state = {
+            selectedDish: null
+
+        }
+    }
+     onDishSelect(dish){
+         this.setState({selectedDish:dish});
+     }
+     renderDish(dish) {
+        if (dish != null)
+            return(
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return(
+                <div></div>
+            );
     }
     render(){
         const menu = this.props.dishes.map((dish)=> {
             return(
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card tag="li">
+                    <Card onClick={()=>
+                        this.onDishSelect(dish)
+                    }>
                             <CardImg width="100%" src={dish.image} alt={dish.name} />
                             <CardImgOverlay heading>
                                 <CardTitle>{dish.name}</CardTitle>
@@ -24,6 +48,11 @@ class MenuComponent extends Component{
             <div className="container">
                 <div className="row">
                         {menu}
+                </div>
+                <div className="row">
+                  <div  className="col-12 col-md-5 m-1">
+                    {this.renderDish(this.state.selectedDish)}
+                  </div>
                 </div>
             </div>
         );
