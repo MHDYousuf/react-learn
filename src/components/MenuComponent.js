02063d4c09942a7b5f96a97d139117c3c08b4382
Mohammed Yousuf <36731361+myousuf07@users.noreply.github.com>
2019-07-02 20:21:23 +0530

    Assignment 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,34 +1,26 @@
-import React, { Component } from 'react'
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap'
-import DishDetail from './DishdetailComponent'
+import React from 'react'
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap'
+import { Link } from 'react-router-dom'
 
-class Menu extends Component{
+function RenderMenuItem ({dish, onClick}) {
+	return (
+		<Card>
+			<Link to = {`/menu/${dish.id}`}>
+				<CardImg width="100%" src={dish.image} alt={dish.name}/>
+					<CardImgOverlay>
+						<CardTitle>{dish.name}</CardTitle>
+					</CardImgOverlay>		
+			</Link>		
+		</Card>		
+	)
+}
 
-	constructor (props) {
-		super(props)
-		this.state = {
-			selectedDish: null
-		}
-	}
-
-	onDishSelect(dish) {
-		this.setState({
-			selectedDish: dish
-		})
-	}
-
-	render() {
-		const dishes = this.props.dishes
+const Menu = (props) => {
+		const dishes = props.dishes
 		const menu = dishes.map((dish) => {
 			return (
 				<div key = {dish.id} className = 'col-12 col-md-5 m-1'>
-					<Card onClick = {()=> this.onDishSelect(dish)}>
-						<CardImg width="100%" src={dish.image} alt={dish.name}/>
-						<CardImgOverlay>
-							<CardTitle>{dish.name}</CardTitle>
-						</CardImgOverlay>
-						
-					</Card>
+					<RenderMenuItem dish = {dish}/>
 				</div>
 			)
 		})
@@ -36,12 +28,26 @@ class Menu extends Component{
 		return (
 			<div className = 'container'>
 				<div className = 'row'>
+					<Breadcrumb>
+						<BreadcrumbItem>
+							<Link to='/home'>Home</Link>
+						</BreadcrumbItem>
+						<BreadcrumbItem active>
+							Menu
+						</BreadcrumbItem>
+					</Breadcrumb>
+					<div className = 'col-12'>
+						<h3>Menu</h3>
+						<hr/>
+					</div>
+				</div>
+
+				<div className = 'row'>
 					{menu}
 				</div>
-				<DishDetail selectedDish = {this.state.selectedDish}/>
 			</div>
 		)
-	}
+
 }
 
 export default Menu 
