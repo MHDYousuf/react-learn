6e8d7ece13b9088862bfd28d402232c4749fb86f
Mohammed Yousuf <36731361+myousuf07@users.noreply.github.com>
2019-07-10 20:31:33 +0530

    Assignment 3

---
+++ src/redux/configureStore.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers } from 'redux'
+import { Dishes } from './dishes'
+import { Promotions } from './promotions'
+import { Leaders } from './leaders'
+import { Comments } from './comments'
+
+export const ConfigureStore = () => {
+	const store = createStore(
+		combineReducers({
+			dishes: Dishes,
+			comments: Comments,
+			promotions: Promotions,
+			leaders: Leaders
+		})
+	)
+
+	return store
+}
